a565ebff840c25960d160103e2e6008391c8a98a
liulx20 <liulexiao.llx@alibaba-inc.com>
2024-11-13 15:17:05 +0800

    fix

--- flex/engines/graph_db/app/batch_update_app.cc
+++ flex/engines/graph_db/app/batch_update_app.cc
@@ -57,62 +57,64 @@ bool BatchUpdateApp::Query(GraphDBSession& graph, Decoder& input,
   grape::OutArchive oarc(std::move(iarc));
   UpdateBatch batch;
   int8_t type;
-  oarc >> type;
+  while (!oarc.Empty()) {
+    oarc >> type;
 
-  if (type == 0) {
-    label_t label;
-    oarc >> label;
-    auto pk = schema.get_vertex_primary_key(label);
-    CHECK(pk.size() == 1) << "Only support single primary key";
-    auto pk_type = std::get<0>(pk[0]);
-    Any oid;
-    oid.type = pk_type;
-    deserialize(oarc, oid);
-    std::vector<Any> props;
-    auto properties = schema.get_vertex_properties(label);
-    for (auto& prop : properties) {
-      Any p;
-      p.type = prop;
-      deserialize(oarc, p);
-      props.push_back(p);
-    }
-    batch.AddVertex(label, std::move(oid), std::move(props));
-  } else {
-    label_t src_label, dst_label, edge_label;
-    Any src, dst;
-    oarc >> src_label;
-    auto src_pk = schema.get_vertex_primary_key(src_label);
-    CHECK(src_pk.size() == 1) << "Only support single primary key";
-    auto src_pk_type = std::get<0>(src_pk[0]);
-    src.type = src_pk_type;
-    deserialize(oarc, src);
-    oarc >> dst_label;
-    auto dst_pk = schema.get_vertex_primary_key(dst_label);
-    CHECK(dst_pk.size() == 1) << "Only support single primary key";
-    auto dst_pk_type = std::get<0>(dst_pk[0]);
-    dst.type = dst_pk_type;
-    deserialize(oarc, dst);
-    oarc >> edge_label;
-    Any value;
-    auto edge_prop =
-        schema.get_edge_properties(src_label, dst_label, edge_label);
-    if (edge_prop.size() == 1) {
-      value.type = edge_prop[0];
-      deserialize(oarc, value);
-    } else {
-      size_t size;
-      oarc >> size;
+    if (type == 0) {
+      label_t label;
+      oarc >> label;
+      auto pk = schema.get_vertex_primary_key(label);
+      CHECK(pk.size() == 1) << "Only support single primary key";
+      auto pk_type = std::get<0>(pk[0]);
+      Any oid;
+      oid.type = pk_type;
+      deserialize(oarc, oid);
       std::vector<Any> props;
-      for (size_t i = 0; i < size; ++i) {
+      auto properties = schema.get_vertex_properties(label);
+      for (auto& prop : properties) {
         Any p;
-        p.type = edge_prop[i];
+        p.type = prop;
         deserialize(oarc, p);
         props.push_back(p);
       }
-      value = Any(props);
+      batch.AddVertex(label, std::move(oid), std::move(props));
+    } else {
+      label_t src_label, dst_label, edge_label;
+      Any src, dst;
+      oarc >> src_label;
+      auto src_pk = schema.get_vertex_primary_key(src_label);
+      CHECK(src_pk.size() == 1) << "Only support single primary key";
+      auto src_pk_type = std::get<0>(src_pk[0]);
+      src.type = src_pk_type;
+      deserialize(oarc, src);
+      oarc >> dst_label;
+      auto dst_pk = schema.get_vertex_primary_key(dst_label);
+      CHECK(dst_pk.size() == 1) << "Only support single primary key";
+      auto dst_pk_type = std::get<0>(dst_pk[0]);
+      dst.type = dst_pk_type;
+      deserialize(oarc, dst);
+      oarc >> edge_label;
+      Any value;
+      auto edge_prop =
+          schema.get_edge_properties(src_label, dst_label, edge_label);
+      if (edge_prop.size() == 1) {
+        value.type = edge_prop[0];
+        deserialize(oarc, value);
+      } else {
+        size_t size;
+        oarc >> size;
+        std::vector<Any> props;
+        for (size_t i = 0; i < size; ++i) {
+          Any p;
+          p.type = edge_prop[i];
+          deserialize(oarc, p);
+          props.push_back(p);
+        }
+        value = Any(props);
+      }
+      batch.AddEdge(src_label, std::move(src), dst_label, std::move(dst),
+                    edge_label, std::move(value));
     }
-    batch.AddEdge(src_label, std::move(src), dst_label, std::move(dst),
-                  edge_label, std::move(value));
   }
   graph.BatchUpdate(batch);
   return true;
